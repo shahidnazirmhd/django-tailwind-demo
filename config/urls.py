ef9572f50a02773d3153162f65c40e2088938af1
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-08-09 12:41:25 +0000

    configuration for serving media files in dev

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView  
+from django.conf.urls.static import static
 
 from django.conf import settings
 
@@ -32,3 +33,5 @@ if settings.DEBUG:
     urlpatterns += [
         path("__reload__/", include("django_browser_reload.urls")),
     ]
+    # Serve media files in dev
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
